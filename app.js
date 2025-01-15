ab148a05f245e2c26a93b55e84fa61c4cf107543
Ezzedine Al Ozon <ezzedineozone123@gmail.com>
2025-01-15 18:03:42 +0200

    changes

--- app.js
+++ app.js
@@ -51,8 +51,7 @@ app.get('/data', (req, res) => {
     });
 });
 app.delete('/data', (req, res) => {
-    console.log(req);
-    const imageTitle = req.body.imageTitle;
+    const imageTitle = req.query.imageTitle;
     const directoryPath = path.join(__dirname, 'uploads');
     const filePath = path.join(directoryPath, `${imageTitle}`);
 
